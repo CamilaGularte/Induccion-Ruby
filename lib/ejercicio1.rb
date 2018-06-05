42f9b017d7fc228dafe40ed9ae31e7659b7baa97
Mooveit <mooveit@MacBook-Pro-de-Mooveit.local>
2018-06-05 17:32:14 -0300

    Exercise 1

--- lib/ejercicio1.rb
+++ lib/ejercicio1.rb
@@ -2,7 +2,7 @@ module Exercise1
   def self.words(*words)
     biggest = words.first
     words.each do |word|
-      word.length > biggest.length ? biggest = word : biggest
+      biggest = word if word.length > biggest.length
     end
     biggest
   end
--- spec/lib/ejercicio1_spec.rb
+++ spec/lib/ejercicio1_spec.rb
@@ -4,24 +4,24 @@ describe Exercise1 do
   subject { Exercise1.words(*words) }
 
   describe '.words' do
-    context 'What happen when one word is longest than the others' do
+    context 'When word is longest than the others' do
       let(:words) { %w[Camila Valentina Matias] }
-      it 'Return the longest word' do
+      it 'Returns the longest word' do
         expect(subject).to eq('Valentina')
       end
     end
   end
 
-  context 'What happen when two or more words have the same longest' do
+  context 'When two or more words have the same longest' do
     let(:words) { %w[123456 Lluvia] }
-    it 'Return the first word entered' do
+    it 'Returns the first word entered' do
       expect(subject).to eq('123456')
     end
   end
 
-  context 'What happen when do not put anything' do
+  context 'When do not put anything' do
     let(:words) { [] }
-    it 'What happen when do not put anything' do
+    it 'When do not put anything' do
       expect(subject).to eq(nil)
     end
   end
