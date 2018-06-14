3b152fd2e6a7c479acfe4fcf01d9307b9b453327
Mooveit <mooveit@MacBook-Pro-de-Mooveit.local>
2018-06-14 17:08:09 -0300

    Exercise 4

--- lib/ejercicio4.rb
+++ lib/ejercicio4.rb
@@ -2,11 +2,10 @@ module Exercise4
   def self.take(array, num)
     return array if num >= array.length
     raise ArgumentError, 'attempt to take negative size' if num < 0
-    return [] if num.zero?
-    array1 = []
+    new_array = []
     num.times do |i|
-      array1.push(array[i])
+      new_array.push(array[i])
     end
-    array1
+    new_array
   end
 end
--- spec/lib/ejercicio4_spec.rb
+++ spec/lib/ejercicio4_spec.rb
@@ -4,31 +4,31 @@ describe Exercise4 do
     subject { Exercise4.take(array, amount) }
     let(:array) { %w[hola como estas] }
 
-    context 'When amount is bigger than array ' do
+    context 'when amount is bigger than array ' do
       let(:amount) { 5 }
       it 'returns all the array' do
         expect(subject).to eq(%w[hola como estas])
       end
     end
-    context 'When amount is equals than array' do
+    context 'when amount is equals than array' do
       let(:amount) { 3 }
       it 'Returns all the array' do
         expect(subject).to eq(%w[hola como estas])
       end
     end
-    context 'When amount is smaller than array' do
+    context 'when amount is smaller than array' do
       let(:amount) { 1 }
       it 'Returns the first object' do
         expect(subject).to eq(%w[hola])
       end
     end
-    context 'When amount is negative' do
+    context 'when amount is negative' do
       let(:amount) { -9 }
       it 'Returns error' do
         expect { subject }.to raise_error(ArgumentError)
       end
     end
-    context 'When amount is negative' do
+    context 'when amount is negative' do
       let(:amount) { 0 }
       it 'Returns error' do
         expect(subject).to eq([])
